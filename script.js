820291d8f782620b7f847ed04337828cf8d451bf
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-11 23:47:46 +0100

    a

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ function createFood() {
 
 function drawFood() {
   context.fillStyle = foodColor
-  context.fillRect(foodX, foodY, unitSize, unitSize);
+  context.fillRect(foodX, foodY, unitSize, unitSize)
 }
 
 function gameStart() {
