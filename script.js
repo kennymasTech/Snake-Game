f6f2eda6d33eaaf88f315922b19589d9101d35ac
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-11 23:53:42 +0100

    a

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 // const gameCont = document.getElementById("gameContainer");
 const gameBoard = document.getElementById("gameBoard");
-const context = gameBoard.getContext("2d")
+const context = gameBoard.getContext("2d");
 const scoreText = document.getElementById("scoreText")
 const resetBtn = document.getElementById("resetBtn")
 const gameWidth = gameBoard.width
